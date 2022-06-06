41e8659954889615b687536bde60607c5a0d3492
Egor <96371791+Zrossiz@users.noreply.github.com>
2022-06-06 13:56:49 +0300

    final

--- source/script.js
+++ source/script.js
@@ -2,13 +2,13 @@
 let navMain = document.querySelector('.header-nav');
 let navToggle = document.querySelector('.header-nav__toggle');
 
-navMain.classList.remove('header-nav--open');
+navMain.classList.remove('header-nav__open');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('header-nav')) {
-    navMain.classList.add('header-nav--open');
+    navMain.classList.add('header-nav__open');
   } else {
-    navMain.classList.add('header-nav--close');
-    navMain.classList.remove('header-nav--open');
+    navMain.classList.add('header-nav__close');
+    navMain.classList.remove('header-nav__open');
   }
 });
